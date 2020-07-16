5a6250855752d51f33783c6e93a2e26913b01b4f
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2020-07-16 16:16:44 +0100

    type writing effect on homepage

--- src/Containers/Home/Homepage.js
+++ src/Containers/Home/Homepage.js
@@ -4,9 +4,17 @@ import "./homepage.css"
 
 import Background from '../../Componants/Particle/Background';
 import Social from '../../Componants/Socials/Social';
-
+import Typer from "../../Componants/Typewriter/Typewriter"
 export class Homepage extends Component {
+
+
+   
+
+   
+
+
     render() {
+        
         return (
             <div className="home-area">
                 <div className="home-particle">
@@ -16,7 +24,12 @@ export class Homepage extends Component {
                     <Row className="justify-content-center">
                         <Col col="12" lg="10">
                             <div className="home-content">
-                            <h1>Hi, I am <span className="color-theme">Calvin Donaldson</span>
+                            <h1>Hi, I am <span> </span>
+                                <Typer
+                                    
+                                    dataText={[" Calvin Donaldson"," Web Developer"," Self-taught"]} 
+                                />
+                                    <span class="cursor"></span>
                             </h1>
                             <p>I'm a web developer from the UK. I love to create beautiful, user-friendly, high-converting websites!</p>
                                 <Social size="3x"/>
