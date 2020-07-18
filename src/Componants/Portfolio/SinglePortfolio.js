09a3f1e63d1232c103ba4c1aa4172e87a594478a
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2020-07-18 14:59:12 +0100

    portfolio page updates

--- src/Componants/Portfolio/SinglePortfolio.js
+++ src/Componants/Portfolio/SinglePortfolio.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faLink} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import {Col} from "react-bootstrap";
 import "./portfolio.css"
 
@@ -8,23 +12,35 @@ export class SinglePortfolio extends Component {
             <Col lg="6" md="6" col="12" className="mt-30">
                 <div class="portfolio-single">
                     <div class="portfolio-img">
-                        <a href="https://clockwise.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <a href={this.props.link} target="_blank" rel="noopener noreferrer">
                         <img src={this.props.image} alt="Clockwise-tech" />
 
                         </a>
                     </div>
-                        <a href="http://heuristic-fermat-36eb80.netlify.app/" target="_blank" rel="noopener noreferrer">
-                        <h5>Clockwise Technologies</h5>
+                        <a href={this.props.link} target="_blank" rel="noopener noreferrer">
+                    <h5>{this.props.title}</h5>
 
                         </a>
-                        <h6>This was a freelance project I did for an engineering firm. It was build using react.js. </h6>
+                            <h6>{this.props.disc}</h6>
                         <div className="tagContainer">
                             <span className="tag">Javascript</span>
                             <span className="tag">HTML</span>
                             <span className="tag">CSS</span>
                             <span className="tag">React</span>
                         </div>
+                        <div className="typeContainer">
+                        <span className="typeTag">{this.props.type} </span>
 
+                        <div className="gitLink">
+                            <a href={this.props.link} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faLink} size="2x" />
+                            </a>
+                            <a href={this.props.repo} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithub} size="2x" />
+                            </a>
+                        </div>
+                        </div>
+                        
                 </div>
                 
             </Col>
--- src/Containers/Portfolio/Portfolio.js
+++ src/Containers/Portfolio/Portfolio.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {Container, Row} from "react-bootstrap";
 import SinglePortfolio from '../../Componants/Portfolio/SinglePortfolio';
 import Clockwise from "../../images/clockwise-services.jpg"
+import MovieApp from "../../images/calvinscrop.png"
 
 export class Portfolio extends Component {
     render() {
@@ -13,8 +14,22 @@ export class Portfolio extends Component {
                         <span>Portfolio</span>
                     </div>
                     <Row>
-                        <SinglePortfolio image={Clockwise}/>
-
+                        <SinglePortfolio 
+                        image={Clockwise}
+                        title="Clockwise Technologies"
+                        disc="This was a freelance project I did for an engineering firm. It was build using react.js."
+                        link="https://clockwise.netlify.app"
+                        repo="https://github.com/calvin1337/portfolio"
+                        type="Professional"
+                        />
+                        <SinglePortfolio 
+                        image={MovieApp} 
+                        title="Calvin's Movie App"
+                        disc="This Was a little project I did to track and rate movies I watched during lockdown using react.js"
+                        link="https://calvinsmovieratings.netlify.app"
+                        repo="https://github.com/calvin1337/MovieRatingApp"
+                        type="Personal"
+                        />
                     </Row>
                 </Container>
             </div>
